52681b992234cca46a4c420f26ce9fdeb1280c9f
komoping <ssps5973123@gmaul.com>
2021-11-04 20:18:20 +0800

    c++_test2_11/4

---
+++ src/example/src/C_two.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ros/ros.h>
+#include <string>
+#include <iostream>
+using namespace std;
+int main(int argc,char **argv)
+{
+        ros::init(argc,argv,"C_two");
+        ros::NodeHandle nh;
+        
+        char array1[100]="hello";
+        char array2[10]="world";
+        strcat(array1,array2);
+        printf("%s\n",array1);
+
+
+        char a1[20]="hello";
+        char a2[20]="world";
+        strcpy(a1,a2);
+        printf("%s\n",a1);
+
+        char a3[20]="hello";
+        int i=0;
+        i=strlen(a1);
+        printf("%d\n",i);
+        return 0;
+}
